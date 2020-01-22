e405cf0b421972398038915c9bbaaf2f7215e59c
Isu <s.georgiadis.94@gmail.com>
2020-01-22 16:09:20 +1030

    just have to implement the timer

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,6 +3,10 @@ var saveScoreBtn = document.getElementById("saveScoreBtn");
 var finalScore = document.getElementById("finalScore");
 var mostRecentScore = localStorage.getItem("mostRecentScore");
 
+var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", function() {
@@ -12,4 +16,16 @@ username.addEventListener("keyup", function() {
 function saveHighScore(e) {
     console.log("Clicked the save button!");
     e.preventDefault();
-};
+
+    var score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+
+    highScores.push(score);
+    highScores.sort(function(a, b){b.score - a.score});
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("../../index.html");
+}
